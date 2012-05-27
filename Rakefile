b726d3b18c48eaad32b41c413882999d3c2d7093
Andrew Jones <akjones@thoughtworks.com>
2012-05-27 18:41:39 +1000

    Tweaked script to call rake task when launch code received via serial

---
+++ Rakefile
@@ -0,0 +1,10 @@
+require 'rake'
+
+task :default => [:deploy]
+
+desc "Deploy the thing"
+task :deploy do
+  puts "Deploying the thing..."
+
+  puts "The thing has been deployed."
+end
--- deploy-button.rb
+++ deploy-button.rb
@@ -1,6 +1,11 @@
 require 'serialport'
+require 'rake'
 
-port_str = ""
+load File.expand_path 'Rakefile'
+
+launch_code = "8iAWv1dVOywXNobylMid"
+
+port_str = "/dev/tty.usbserial-A6008lLX"
 baud_rate = 9600
 data_bits = 8
 stop_bits = 1
@@ -9,7 +14,19 @@ parity = SerialPort::NONE
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 while true do
-  printf("%c", sp.getc)
+  line = ""
+  c = sp.getc
+  while c != "\n" do
+    line << c
+    c = sp.getc
+  end
+
+  if line == launch_code 
+    puts "Launch codes received"
+    Rake::Task["deploy"].invoke
+    Rake::Task["deploy"].reenable
+    sleep 1
+  end 
 end
 
 sp.close

7750865781aa3051ba284f4875e6c96fb51fa08a
Andrew Jones <andrew@andrewjones.id.au>
2012-10-28 15:10:23 +1100

    New directory structure

---
+++ firmware/deploy_button.ino
@@ -0,0 +1,36 @@
+const int buttonPin = 3;
+const int ledPin =  13;
+const char* launchCode = "8iAWv1dVOywXNobylMid\n";
+
+int buttonState = 0;
+int ledState = HIGH;
+int lastButtonState = HIGH;
+
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      
+  pinMode(buttonPin, INPUT);   
+  Serial.begin(9600); 
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+    if (buttonState == LOW) {
+      Serial.println(launchCode);
+    }
+  }  
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    buttonState = reading;
+  }
+
+  digitalWrite(ledPin, buttonState);  
+
+  lastButtonState = reading;
+}
+

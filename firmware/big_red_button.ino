f740af5215be8fdde650099e3447dd91ffffe26f
Andrew Jones <andrew@andrewjones.id.au>
2012-10-28 19:41:46 +1100

    Rename arduino file to match project name

--- firmware/big_red_button.ino
+++ firmware/big_red_button.ino
@@ -1,18 +1,17 @@
 const int buttonPin = 3;
-const int ledPin =  13;
+const int ledPin = 5;
 const char* launchCode = "8iAWv1dVOywXNobylMid\n";
 
-int buttonState = 0;
-int ledState = HIGH;
+int buttonState = 0; int ledState = HIGH;
 int lastButtonState = HIGH;
 
 long lastDebounceTime = 0;
 long debounceDelay = 50;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);      
-  pinMode(buttonPin, INPUT);   
-  Serial.begin(9600); 
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -23,14 +22,13 @@ void loop() {
     if (buttonState == LOW) {
       Serial.println(launchCode);
     }
-  }  
+  }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     buttonState = reading;
   }
 
-  digitalWrite(ledPin, buttonState);  
+  digitalWrite(ledPin, buttonState);
 
   lastButtonState = reading;
 }
-
